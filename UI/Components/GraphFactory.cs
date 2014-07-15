42c2aa14bb4d429802a33845c35fd234bbcdd565
CryZe <cryze92@gmail.com>
2014-07-16 00:12:18 +0200

    Updated the Description

--- UI/Components/GraphFactory.cs
+++ UI/Components/GraphFactory.cs
@@ -17,7 +17,7 @@ namespace LiveSplit.UI.Components
 
         public string Description
         {
-            get { return ""; }
+            get { return "Shows a graph of the current run in relation to a comparison."; }
         }
 
         public ComponentCategory Category
