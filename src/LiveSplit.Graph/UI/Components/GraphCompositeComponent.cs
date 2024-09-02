5555cfe4dde5cc12994e4b50916c5c474c11e954
Ero <imjustero@gmail.com>
2024-09-02 10:07:08 +0200

    Use file-scoped namespaces, move `using` directives, fix whitespace
    * `IDE0005`, `IDE0065`, `IDE0161`

--- src/LiveSplit.Graph/UI/Components/GraphCompositeComponent.cs
+++ src/LiveSplit.Graph/UI/Components/GraphCompositeComponent.cs
@@ -1,98 +1,98 @@
-﻿using LiveSplit.Model;
-using LiveSplit.Model.Comparisons;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace LiveSplit.UI.Components
+using LiveSplit.Model;
+using LiveSplit.Model.Comparisons;
+
+namespace LiveSplit.UI.Components;
+
+public class GraphCompositeComponent : IComponent
 {
-    public class GraphCompositeComponent : IComponent
+    protected GraphSettings Settings { get; set; }
+    public ComponentRendererComponent InternalComponent { get; protected set; }
+
+    public float PaddingTop => InternalComponent.PaddingTop;
+    public float PaddingLeft => InternalComponent.PaddingLeft;
+    public float PaddingBottom => InternalComponent.PaddingBottom;
+    public float PaddingRight => InternalComponent.PaddingRight;
+
+    public IDictionary<string, Action> ContextMenuControls => null;
+
+    public GraphCompositeComponent(LiveSplitState state)
     {
-        protected GraphSettings Settings { get; set; }
-        public ComponentRendererComponent InternalComponent { get; protected set; }
-
-        public float PaddingTop => InternalComponent.PaddingTop;
-        public float PaddingLeft => InternalComponent.PaddingLeft;
-        public float PaddingBottom => InternalComponent.PaddingBottom;
-        public float PaddingRight => InternalComponent.PaddingRight;
-
-        public IDictionary<string, Action> ContextMenuControls => null;
-
-        public GraphCompositeComponent(LiveSplitState state)
+        Settings = new GraphSettings()
         {
-            Settings = new GraphSettings()
-            {
-                CurrentState = state
-            };
-            InternalComponent = new ComponentRendererComponent();
-            var components = new List<IComponent>();
-            components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = true });
-            components.Add(new GraphComponent(Settings));
-            components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = false });
-            InternalComponent.VisibleComponents = components;
-            state.ComparisonRenamed += state_ComparisonRenamed;
-        }
-
-        void state_ComparisonRenamed(object sender, EventArgs e)
-        {
-            var args = (RenameEventArgs)e;
-            if (Settings.Comparison == args.OldName)
-            {
-                Settings.Comparison = args.NewName;
-                ((LiveSplitState)sender).Layout.HasChanged = true;
-            }
-        }
-
-        public Control GetSettingsControl(LayoutMode mode)
-        {
-            Settings.Mode = mode;
-            return Settings;
-        }
-
-        public void SetSettings(System.Xml.XmlNode settings)
-        {
-            Settings.SetSettings(settings);
-        }
-
-        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
-        {
-            return Settings.GetSettings(document);
-        }
-
-        public string ComponentName
-            => "Graph" + (Settings.Comparison == "Current Comparison" 
-                ? "" 
-                : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
-
-        public float HorizontalWidth => InternalComponent.HorizontalWidth;
-
-        public float MinimumHeight => InternalComponent.MinimumHeight;
-
-        public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, Region clipRegion)
-        {
-            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
-        }
-
-        public float VerticalHeight => InternalComponent.VerticalHeight;
-
-        public float MinimumWidth => InternalComponent.MinimumWidth;
-
-        public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, Region clipRegion)
-        {
-            InternalComponent.DrawVertical(g, state, width, clipRegion);
-        }
-
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-        {
-            if (invalidator != null)
-                InternalComponent.Update(invalidator, state, width, height, mode);
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+            CurrentState = state
+        };
+        InternalComponent = new ComponentRendererComponent();
+        var components = new List<IComponent>();
+        components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = true });
+        components.Add(new GraphComponent(Settings));
+        components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = false });
+        InternalComponent.VisibleComponents = components;
+        state.ComparisonRenamed += state_ComparisonRenamed;
     }
+
+    void state_ComparisonRenamed(object sender, EventArgs e)
+    {
+        var args = (RenameEventArgs)e;
+        if (Settings.Comparison == args.OldName)
+        {
+            Settings.Comparison = args.NewName;
+            ((LiveSplitState)sender).Layout.HasChanged = true;
+        }
+    }
+
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        Settings.Mode = mode;
+        return Settings;
+    }
+
+    public void SetSettings(System.Xml.XmlNode settings)
+    {
+        Settings.SetSettings(settings);
+    }
+
+    public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+    {
+        return Settings.GetSettings(document);
+    }
+
+    public string ComponentName
+        => "Graph" + (Settings.Comparison == "Current Comparison"
+            ? ""
+            : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
+
+    public float HorizontalWidth => InternalComponent.HorizontalWidth;
+
+    public float MinimumHeight => InternalComponent.MinimumHeight;
+
+    public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+    }
+
+    public float VerticalHeight => InternalComponent.VerticalHeight;
+
+    public float MinimumWidth => InternalComponent.MinimumWidth;
+
+    public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        InternalComponent.DrawVertical(g, state, width, clipRegion);
+    }
+
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        if (invalidator != null)
+            InternalComponent.Update(invalidator, state, width, height, mode);
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
 }
--- src/LiveSplit.Graph/UI/Components/GraphSeparatorComponent.cs
+++ src/LiveSplit.Graph/UI/Components/GraphSeparatorComponent.cs
@@ -1,110 +1,110 @@
-﻿using LiveSplit.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
 
-namespace LiveSplit.UI.Components
+using LiveSplit.Model;
+
+namespace LiveSplit.UI.Components;
+
+public class GraphSeparatorComponent : IComponent
 {
-    public class GraphSeparatorComponent : IComponent
+    protected LineComponent Line { get; set; }
+    protected GraphSettings Settings { get; set; }
+
+    public bool LockToBottom { get; set; }
+
+    public float PaddingTop => 0f;
+    public float PaddingBottom => 0f;
+    public float PaddingLeft => 0f;
+    public float PaddingRight => 0f;
+
+    public GraphicsCache Cache { get; set; }
+
+    public float VerticalHeight => 1f;
+
+    public float MinimumWidth => 0f;
+
+    public IDictionary<string, Action> ContextMenuControls => null;
+
+    public GraphSeparatorComponent(GraphSettings settings)
     {
-        protected LineComponent Line { get; set; }
-        protected GraphSettings Settings { get; set; }
+        Line = new LineComponent(1, Color.White);
+        Settings = settings;
+        Cache = new GraphicsCache();
+    }
 
-        public bool LockToBottom { get; set; }
+    public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        var oldClip = g.Clip;
+        var oldMatrix = g.Transform;
+        var oldMode = g.SmoothingMode;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+        g.Clip = new Region();
+        Line.LineColor = Settings.GraphLinesColor;
+        var scale = g.Transform.Elements.First();
+        var newHeight = Math.Max((int)(1f * scale + 0.5f), 1) / scale;
+        Line.VerticalHeight = newHeight;
+        if (LockToBottom)
+            g.TranslateTransform(0, 1f - newHeight);
+        Line.DrawVertical(g, state, width, clipRegion);
+        g.Clip = oldClip;
+        g.Transform = oldMatrix;
+        g.SmoothingMode = oldMode;
+    }
 
-        public float PaddingTop => 0f;
-        public float PaddingBottom => 0f;
-        public float PaddingLeft => 0f;
-        public float PaddingRight => 0f;
+    public string ComponentName => "Graph Separator";
 
-        public GraphicsCache Cache { get; set; }
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        throw new NotSupportedException();
+    }
 
-        public float VerticalHeight => 1f;
+    public XmlNode GetSettings(XmlDocument document)
+    {
+        throw new NotSupportedException();
+    }
 
-        public float MinimumWidth => 0f;
+    public void SetSettings(XmlNode settings)
+    {
+        throw new NotSupportedException();
+    }
 
-        public IDictionary<string, Action> ContextMenuControls => null;
+    public float HorizontalWidth => 1f;
 
-        public GraphSeparatorComponent(GraphSettings settings)
-        {
-            Line = new LineComponent(1, Color.White);
-            Settings = settings;
-            Cache = new GraphicsCache();
-        }
+    public float MinimumHeight => 0f;
 
-        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
-        {
-            var oldClip = g.Clip;
-            var oldMatrix = g.Transform;
-            var oldMode = g.SmoothingMode;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
-            g.Clip = new Region();
-            Line.LineColor = Settings.GraphLinesColor;
-            var scale = g.Transform.Elements.First();
-            var newHeight = Math.Max((int)(1f * scale + 0.5f), 1) / scale;
-            Line.VerticalHeight = newHeight;
-            if (LockToBottom)
-                g.TranslateTransform(0, 1f - newHeight);
-            Line.DrawVertical(g, state, width, clipRegion);
-            g.Clip = oldClip;
-            g.Transform = oldMatrix;
-            g.SmoothingMode = oldMode;
-        }
+    public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        var oldClip = g.Clip;
+        var oldMatrix = g.Transform;
+        var oldMode = g.SmoothingMode;
+        g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+        g.Clip = new Region();
+        Line.LineColor = Settings.GraphLinesColor;
+        var scale = g.Transform.Elements.First();
+        var newWidth = Math.Max((int)(1f * scale + 0.5f), 1) / scale;
+        if (LockToBottom)
+            g.TranslateTransform(1f - newWidth, 0);
+        Line.HorizontalWidth = newWidth;
+        Line.DrawHorizontal(g, state, height, clipRegion);
+        g.Clip = oldClip;
+        g.Transform = oldMatrix;
+        g.SmoothingMode = oldMode;
+    }
 
-        public string ComponentName => "Graph Separator";
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        Cache.Restart();
+        Cache["LockToBottom"] = LockToBottom;
 
-        public Control GetSettingsControl(LayoutMode mode)
-        {
-            throw new NotSupportedException();
-        }
+        if (invalidator != null && Cache.HasChanged)
+            invalidator.Invalidate(0, 0, width, height);
+    }
 
-        public XmlNode GetSettings(XmlDocument document)
-        {
-            throw new NotSupportedException();
-        }
-
-        public void SetSettings(XmlNode settings)
-        {
-            throw new NotSupportedException();
-        }
-
-        public float HorizontalWidth => 1f;
-
-        public float MinimumHeight => 0f;
-
-        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
-        {
-            var oldClip = g.Clip;
-            var oldMatrix = g.Transform;
-            var oldMode = g.SmoothingMode;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
-            g.Clip = new Region();
-            Line.LineColor = Settings.GraphLinesColor;
-            var scale = g.Transform.Elements.First();
-            var newWidth = Math.Max((int)(1f * scale + 0.5f), 1) / scale;
-            if (LockToBottom)
-                g.TranslateTransform(1f - newWidth, 0);
-            Line.HorizontalWidth = newWidth;
-            Line.DrawHorizontal(g, state, height, clipRegion);
-            g.Clip = oldClip;
-            g.Transform = oldMatrix;
-            g.SmoothingMode = oldMode;
-        }
-
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-        {
-            Cache.Restart();
-            Cache["LockToBottom"] = LockToBottom;
-
-            if (invalidator != null && Cache.HasChanged)
-                invalidator.Invalidate(0, 0, width, height);
-        }
-
-        public void Dispose()
-        {
-        }
+    public void Dispose()
+    {
     }
 }
