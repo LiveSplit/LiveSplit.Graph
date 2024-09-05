eb317448fdd5868c4b5f6fd4d738fbc8a773841f
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:16 +0200

    Add `.editorconfig` & apply style rules (#13)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Add and order modifiers
    
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Use object initializer, collection initializer/expression
    
    * `IDE0017`, `IDE0028`, `IDE0300`, `IDE0301`, `IDE0302`, `IDE0303`, `IDE0304`, `IDE0305`
    
    * Use pattern matching
    
    * `IDE0019`, `IDE0020`, `IDE0038`, `IDE0078`, `IDE0083`, `IDE0170`, `IDE0260`
    
    * Inline variable declaration
    
    * `IDE0018`
    
    * Simplify qualifications
    
    * `IDE0001`, `IDE0002`, `IDE0003`, `IDE0034`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build
    
    * Fix naming violations

--- src/LiveSplit.Graph/UI/Components/GraphCompositeComponent.cs
+++ src/LiveSplit.Graph/UI/Components/GraphCompositeComponent.cs
@@ -1,98 +1,105 @@
-﻿using LiveSplit.Model;
-using LiveSplit.Model.Comparisons;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace LiveSplit.UI.Components
+using LiveSplit.Model;
+using LiveSplit.Model.Comparisons;
+
+namespace LiveSplit.UI.Components;
+
+public class GraphCompositeComponent : IComponent
 {
-    public class GraphCompositeComponent : IComponent
+    protected GraphSettings Settings { get; set; }
+    public ComponentRendererComponent InternalComponent { get; protected set; }
+
+    public float PaddingTop => InternalComponent.PaddingTop;
+    public float PaddingLeft => InternalComponent.PaddingLeft;
+    public float PaddingBottom => InternalComponent.PaddingBottom;
+    public float PaddingRight => InternalComponent.PaddingRight;
+
+    public IDictionary<string, Action> ContextMenuControls => null;
+
+    public GraphCompositeComponent(LiveSplitState state)
     {
-        protected GraphSettings Settings { get; set; }
-        public ComponentRendererComponent InternalComponent { get; protected set; }
-
-        public float PaddingTop => InternalComponent.PaddingTop;
-        public float PaddingLeft => InternalComponent.PaddingLeft;
-        public float PaddingBottom => InternalComponent.PaddingBottom;
-        public float PaddingRight => InternalComponent.PaddingRight;
-
-        public IDictionary<string, Action> ContextMenuControls => null;
-
-        public GraphCompositeComponent(LiveSplitState state)
+        Settings = new GraphSettings()
         {
-            Settings = new GraphSettings()
-            {
-                CurrentState = state
-            };
-            InternalComponent = new ComponentRendererComponent();
-            var components = new List<IComponent>();
-            components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = true });
-            components.Add(new GraphComponent(Settings));
-            components.Add(new GraphSeparatorComponent(Settings) { LockToBottom = false });
-            InternalComponent.VisibleComponents = components;
-            state.ComparisonRenamed += state_ComparisonRenamed;
-        }
-
-        void state_ComparisonRenamed(object sender, EventArgs e)
+            CurrentState = state
+        };
+        InternalComponent = new ComponentRendererComponent();
+        var components = new List<IComponent>
         {
-            var args = (RenameEventArgs)e;
-            if (Settings.Comparison == args.OldName)
-            {
-                Settings.Comparison = args.NewName;
-                ((LiveSplitState)sender).Layout.HasChanged = true;
-            }
-        }
+            new GraphSeparatorComponent(Settings) { LockToBottom = true },
+            new GraphComponent(Settings),
+            new GraphSeparatorComponent(Settings) { LockToBottom = false }
+        };
+        InternalComponent.VisibleComponents = components;
+        state.ComparisonRenamed += state_ComparisonRenamed;
+    }
 
-        public Control GetSettingsControl(LayoutMode mode)
+    private void state_ComparisonRenamed(object sender, EventArgs e)
+    {
+        var args = (RenameEventArgs)e;
+        if (Settings.Comparison == args.OldName)
         {
-            Settings.Mode = mode;
-            return Settings;
+            Settings.Comparison = args.NewName;
+            ((LiveSplitState)sender).Layout.HasChanged = true;
         }
+    }
 
-        public void SetSettings(System.Xml.XmlNode settings)
+    public Control GetSettingsControl(LayoutMode mode)
+    {
+        Settings.Mode = mode;
+        return Settings;
+    }
+
+    public void SetSettings(System.Xml.XmlNode settings)
+    {
+        Settings.SetSettings(settings);
+    }
+
+    public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+    {
+        return Settings.GetSettings(document);
+    }
+
+    public string ComponentName
+        => "Graph" + (Settings.Comparison == "Current Comparison"
+            ? ""
+            : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
+
+    public float HorizontalWidth => InternalComponent.HorizontalWidth;
+
+    public float MinimumHeight => InternalComponent.MinimumHeight;
+
+    public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+    {
+        InternalComponent.DrawHorizontal(g, state, height, clipRegion);
+    }
+
+    public float VerticalHeight => InternalComponent.VerticalHeight;
+
+    public float MinimumWidth => InternalComponent.MinimumWidth;
+
+    public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+    {
+        InternalComponent.DrawVertical(g, state, width, clipRegion);
+    }
+
+    public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+    {
+        if (invalidator != null)
         {
-            Settings.SetSettings(settings);
+            InternalComponent.Update(invalidator, state, width, height, mode);
         }
+    }
 
-        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
-        {
-            return Settings.GetSettings(document);
-        }
+    public void Dispose()
+    {
+    }
 
-        public string ComponentName
-            => "Graph" + (Settings.Comparison == "Current Comparison" 
-                ? "" 
-                : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
-
-        public float HorizontalWidth => InternalComponent.HorizontalWidth;
-
-        public float MinimumHeight => InternalComponent.MinimumHeight;
-
-        public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, Region clipRegion)
-        {
-            InternalComponent.DrawHorizontal(g, state, height, clipRegion);
-        }
-
-        public float VerticalHeight => InternalComponent.VerticalHeight;
-
-        public float MinimumWidth => InternalComponent.MinimumWidth;
-
-        public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, Region clipRegion)
-        {
-            InternalComponent.DrawVertical(g, state, width, clipRegion);
-        }
-
-        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
-        {
-            if (invalidator != null)
-                InternalComponent.Update(invalidator, state, width, height, mode);
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
+    public int GetSettingsHashCode()
+    {
+        return Settings.GetSettingsHashCode();
     }
 }
--- src/LiveSplit.Graph/UI/Components/GraphFactory.cs
+++ src/LiveSplit.Graph/UI/Components/GraphFactory.cs
@@ -1,27 +1,30 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(GraphFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class GraphFactory : IComponentFactory
 {
-    public class GraphFactory : IComponentFactory
+    public string ComponentName => "Graph";
+
+    public string Description => "Shows a graph of the current run in relation to a comparison.";
+
+    public ComponentCategory Category => ComponentCategory.Media;
+
+    public IComponent Create(LiveSplitState state)
     {
-        public string ComponentName => "Graph";
-
-        public string Description => "Shows a graph of the current run in relation to a comparison.";
-
-        public ComponentCategory Category => ComponentCategory.Media;
-
-        public IComponent Create(LiveSplitState state) => new GraphCompositeComponent(state);
-
-        public string UpdateName => ComponentName;
-
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Graph.xml";
-
-        public string UpdateURL => "http://livesplit.org/update/";
-
-        public Version Version => Version.Parse("1.8.29");
+        return new GraphCompositeComponent(state);
     }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.Graph.xml";
+
+    public string UpdateURL => "http://livesplit.org/update/";
+
+    public Version Version => Version.Parse("1.8.29");
 }
